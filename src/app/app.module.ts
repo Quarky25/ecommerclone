cb490391d2dcff0aefde3d118e8b82852153626a
Quarky25 <ralphganser4@gmail.com>
2024-11-08 13:50:41 +0100

    added submit control, and styling to form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormSubmitComponent } from './form-submit/form-submit.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ContactUsComponent,
-    FormSubmitComponent
+   
   ],
   imports: [
     BrowserModule,
