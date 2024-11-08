cf71cf307c2b4663ad3669b01b5b42dec4ce880c
nekyooo <schierta@gmail.com>
2024-11-08 14:01:59 +0100

    added carlist.ts, added carList.model, added car-images in public folder

---
+++ src/app/model/carList.model.ts
@@ -0,0 +1,8 @@
+
+export interface ICarsList{
+    model: string,
+    brand: string,
+    price: number,
+    img: string,
+    desc?: string
+};
---
+++ src/app/services/carList.ts
@@ -0,0 +1,110 @@
+import {ICarsList} from '../model/carList.model'; 
+
+
+export class CarService {
+carList: ICarsList[] = 
+[
+    {
+        model: "Classic",
+        brand: "Bobby Car",
+        price: 750000,
+        img: "./images/img-cars/bobbycar.jpg",
+        desc: "Our greatest offer for big and small!"
+      },
+    {
+        model: "Model S",
+        brand: "Tesla",
+        price: 85000,
+        img: "./images/img-cars/tesla.jpg",
+        desc: "A luxury electric car"
+      },
+      {
+        model: "3 Series",
+        brand: "BMW",
+        price: 30000,
+        img: "./images/img-cars/bmw3.jpg",
+        desc: "A compact executive car"
+      },
+      {
+        model: "Yaris",
+        brand: "Toyota",
+        price: 18000,
+        img: "/images/img-cars/toyota.jpg",
+        desc: "A reliable car"
+      },
+    {
+        model: "Camry",
+        brand: "Toyota",
+        price: 15000,
+        img: "./images/img-cars/car-7153508_640.jpg",
+        desc: ""
+      },
+    {
+        model: "Accord",
+        brand: "Honda",
+        price: 17500,
+        img: "./images/img-cars/2021-honda-accord.jpg",
+        desc: ""
+      },
+      {
+        model: "Evo",
+        brand: "Mitsubishi",
+        price: 45000,
+        img: "/images/img-cars/mitsubishi.jpg",
+        desc: ""
+      },
+      {
+        model: "Multipla",
+        brand: "Fiat",
+        price: 500000,
+        img: "/images/img-cars/multipla.jpg",
+        desc: ""
+      },
+    {
+      model: "GT-R",
+      brand: "Nissan",
+      price: 122000,
+      img: "/images/img-cars/nissanGTR.jpg",
+      desc: ""
+    },
+    {
+      model: "LC",
+      brand: "Lexus",
+      price: 99800,
+      img: "/images/img-cars/lexusLC.jpg",
+      desc: ""
+  
+    }, {
+      model: "G90",
+      brand: "Genesis",
+      price: 90400,
+      img: "/images/img-cars/genesisG90.jpg",
+      desc: ""
+  
+    }, {
+      model: "Corvette",
+      brand: "Chevrolet",
+      price: 106500,
+      img: "/images/img-cars/corvette.jpg",
+      desc: ""
+  
+    }, {
+      model: "RS 6 Avant",
+      brand: "Audi",
+      price: 126900,
+      img: "/images/img-cars/audiRS6.jpg",
+      desc: ""
+  
+    }, {
+      model: "Ghibli",
+      brand: "Maserati",
+      price: 110900,
+      img: "/images/img-cars/MaseratiGhibli.jpg",
+      desc: ""
+
+    },
+  ];
+  getCars(): ICarsList[] {
+    return this.carList;}
+};
+
