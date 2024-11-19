36dd17eca515c23a83e3e73cfba27c0a12c05693
Donarsdottir <vera.katharina.mai@gmail.com>
2024-11-19 15:34:10 +0100

    Modified text, added effect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 const routes: Routes = [
   {path: "", component: HomePageComponent},
   {path: "contact", component: ContactUsComponent},
+  {path: "about", component: AboutUsComponent},
   
 
 ];
