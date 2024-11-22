4c0a661a67c61209dad57739546a7c2a0be8f443
Quarky25 <ralphganser4@gmail.com>
2024-11-22 13:18:48 +0100

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-<<<<<<< HEAD
+
 import { CarDetailsComponent } from './car-details/car-details.component';
-=======
+
 import { CartComponent } from './cart/cart.component';
->>>>>>> 87eeec9c16b116660759528c08dc84b561b05e0a
+
 
 
 
@@ -17,13 +17,10 @@ const routes: Routes = [
   {path: "contact", component: ContactUsComponent},
   {path: "about", component: AboutUsComponent},
   {path: "cars", component: ProductPageComponent},
-<<<<<<< HEAD
-  {path: "details/:id", component: CarDetailsComponent}
+  {path: "details/:id", component: CarDetailsComponent},
 
-=======
   {path: "cart", component: CartComponent},
 {path: "product-page", component: ProductPageComponent},
->>>>>>> 87eeec9c16b116660759528c08dc84b561b05e0a
 ];
 
 @NgModule({
