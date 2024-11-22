0d4152209b8076fdd455b44ef15b946aea399832
Quarky25 <ralphganser4@gmail.com>
2024-11-22 13:17:36 +0100

    added activated route to home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ICarsList, carList } from '../module/carList.modulel';
 import { CarListService } from '../services/car-list.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 
 
@@ -12,12 +13,19 @@ import { CarListService } from '../services/car-list.service';
 })
 export class HomePageComponent {
   cars: ICarsList[] = carList;
-  
+  car: ICarsList = {} as ICarsList
   randCar: ICarsList[] = [] ;
+  index: number = 0;
 
 
-  constructor(private carService: CarListService){
-  
+  constructor(private carService: CarListService, private router: ActivatedRoute){
+  this.router.paramMap.subscribe((params: ParamMap) => {
+    const id = params.get('id');
+    if(id !== null) {
+      this.index = +id;
+      this.car = this.cars[this.index]
+    }
+  })
   this.randCar = this.carService.shuffleArray(this.cars)
   }
   
